9ca14e4168f5f93a7019219c1ef27efbb6262d41
DhiLaurent <79476878+DhiLaurent@users.noreply.github.com>
2022-01-22 23:10:59 -0500

    Update Tohru.py

--- Tohru.py
+++ Tohru.py
@@ -5,48 +5,55 @@ import sys
 
 
 def banner():
-	print("\033[31m---------------------------------------------------")
-	tprint(" Tohru ",font="slant")
-	print("----------------------------------------------------\033[m")
+        print("\033[31m---------------------------------------------------")
+        tprint(" Tohru ",font="slant")
+        print("----------------------------------------------------\033[m")
 
 
 if len(sys.argv) != 2:
-	# Infos
-	print("\033[31m+--------------------------------------------------------+")
-	print("|       Como usar: python3 Tohru.py (WordList)           |")
-	print("|        How to use: python3 Tohru (Wordlist)            |")
-	print("+--------------------------------------------------------+\033[m")
+        # Infos
+        print("\033[31m+--------------------------------------------------------+")
+        print("|       Como usar: python3 Tohru.py (WordList)           |")
+        print("|        How to use: python3 Tohru (Wordlist)            |")
+        print("+--------------------------------------------------------+\033[m")
 
 elif len(sys.argv) == 2:
-	try:
-		from art import *
-		# Banner
-		banner()
-		# Full Hash
-		hash = input("\033[31mHash Completa:\033[m ")
-		# Salt
-		salt = input("\033[31mSalt:\033[m ")
-		print("\n")
-		print("\033[31m			+--------------------------------------------+")
-		print("			|        Cracking hold a second...           |")
-		print("			+--------------------------------------------+\033[m")
-		print("\n")
-		# Opening and reading Wordlist
-		try:
-			with open(sys.argv[1],'r', encoding='iso8859_15') as file:
-				while (line := file.readline().rstrip()):
-					hashsalt = crypt.crypt(line,salt)
-					# if hash match
-					if hashsalt == hash:
-						print(f"\033[31m[HASH]:\033[m {hashsalt} \033[31m\n[SENHA]:\033[m {line}")
-						exit()
-		except Exception as error:
-			print("Wordlist didn't load")
-			print(error)
-	except Exception as error:
-		print("Module not installed")
-		print(error)
-print("Tohru couldn't break the hash :c")
+        try:
+                from art import *
+                # Banner
+                banner()
+                # Full Hash
+                hash = input("\033[31mHash Completa:\033[m ")
+                # Salt
+                salt = input("\033[31mSalt:\033[m ")
+                print("\n")
+                print("\033[31m                 +--------------------------------------------+")
+                print("                 |        Cracking hold a second...           |")
+                print("                 +--------------------------------------------+\033[m")
+                print("\n")
+                # Opening and reading Wordlist
+                try:
+                        with open(sys.argv[1],'r', encoding='iso8859_15') as file:
+                                while (line := file.readline().rstrip()):
+                                        hashsalt = crypt.crypt(line,salt)
+                                        # if hash match
+                                        if hashsalt == hash:
+                                                print(f"\033[31m[HASH]:\033[m {hashsalt} \033[31m\n[SENHA]:\033[m {line}")
+                                                exit()
+                                        #else:
+                                        #       print(hashsalt)
 
+                except Exception as error:
+                        print("\033[31m[*] Wordlist didn't load")
+                        print(error)
+                        exit()
 
-
+        except Exception as error:
+                print("\033[31m[*] Module not installed")
+                print(error)
+                exit()
+print("\033[31m------------------------------------------------------------------------------\n")
+print("                 +--------------------------------------------+")
+print("                 |      Tohru couldn't break the hash :c      |")
+print("                 +--------------------------------------------+")
+print("\033[31m------------------------------------------------------------------------------\n")
